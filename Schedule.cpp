bb323e4894b11d81c8c0dc268a32c1eee29eb845
Leo-lonJ <39234466+Leo-lonJ@users.noreply.github.com>
2019-04-21 21:50:24 +0800

    Add files via upload

--- Schedule.cpp
+++ Schedule.cpp
@@ -33,8 +33,15 @@ void PutSchedule()
 	{
 		while (!in.eof())
 		{
-			string from, to;
-			in >> from >> to;
+			string from;				//起始城市
+			string to;					//到达城市
+			int begin_time;		//开始时间
+			int end_time;			//到达时间
+			int time_used;			//所用时间/分钟
+			int cost;				//费用
+			string traffic;
+			in >> from >> to >> traffic >> begin_time >> end_time >> cost >> time_used;
+			//cout << from << ' ' << to << ' ' << traffic << ' ' << begin_time << ' ' << end_time << ' ' << cost << ' ' << time_used << endl;
 			if (from == "北京") i = 0;
 			else if (from == "上海") i = 1;
 			else if (from == "广州") i = 2;
@@ -47,15 +54,21 @@ void PutSchedule()
 			else if (from == "哈尔滨") i = 9;
 			if (to == "北京") j = 0;
 			else if (to == "上海") j = 1;
-			else if (from == "广州") j = 2;
-			else if (from == "厦门") j = 3;
-			else if (from == "重庆") j = 4;
-			else if (from == "拉萨") j = 5;
-			else if (from == "乌鲁木齐") j = 6;
-			else if (from == "武汉") j = 7;
-			else if (from == "南京") j = 8;
-			else if (from == "哈尔滨") j = 9;
-			in >> shift[i][j].path[shift[i][j].path_number].traffic >> shift[i][j].path[shift[i][j].path_number].begin_time >> shift[i][j].path[shift[i][j].path_number].end_time >> shift[i][j].path[shift[i][j].path_number].cost >> shift[i][j].path[shift[i][j].path_number].time_used;
+			else if (to == "广州") j = 2;
+			else if (to == "厦门") j = 3;
+			else if (to == "重庆") j = 4;
+			else if (to == "拉萨") j = 5;
+			else if (to == "乌鲁木齐") j = 6;
+			else if (to == "武汉") j = 7;
+			else if (to == "南京") j = 8;
+			else if (to == "哈尔滨") j = 9;
+			shift[i][j].path[shift[i][j].path_number].from = from;
+			shift[i][j].path[shift[i][j].path_number].to = to;
+			shift[i][j].path[shift[i][j].path_number].traffic = traffic;
+			shift[i][j].path[shift[i][j].path_number].begin_time = begin_time;
+			shift[i][j].path[shift[i][j].path_number].end_time = end_time;
+			shift[i][j].path[shift[i][j].path_number].cost = cost;
+			shift[i][j].path[shift[i][j].path_number].time_used = time_used;
 			shift[i][j].path_number++;
 		}
 	}
@@ -64,12 +77,16 @@ void PutSchedule()
 		cout << "Erroer:open txt failed" << endl;
 	}
 	in.close();
-	for (i = 0; i < 10; i++)
+	/*for (i = 0; i < 10; i++)
 	{
 		for (j = 0; j < 10; j++)
 		{
-			cout << " " << i << " " << j << " " << shift[i][j].path_number;
+			for (int x = 0; x < shift[i][j].path_number; x++)
+			{
+				cout << shift[i][j].path[x].traffic << endl;
+			}
 		}
 		cout << endl;
-	}
+	}*/
+
 }
