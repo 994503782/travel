c49423297b405bef1ec5f82705b954bc33411f5a
Leo-lonJ <39234466+Leo-lonJ@users.noreply.github.com>
2019-04-21 19:18:52 +0800

    Add files via upload

--- Schedule.cpp
+++ Schedule.cpp
@@ -14,50 +14,62 @@
 
 void PutSchedule()
 {
-	Schedule **shift;
 	int i, j;
-	Schedule shift = new Schedule*[10];
-	for (i=0; i<10 ; i++)
+	Schedule **shift = new Schedule*[10];
+	for (i = 0; i < 10; i++)
 	{
 		shift[i] = new Schedule[10];
 	}
-	ifstream fin("pathdata.txt");
-	if (!fin.is_open())
+	for (i = 0; i < 10; i++)
 	{
-		cout<<"Error: opening file fail"<<endl;
-		exit(1);
+		for (j = 0; j < 10; j++)
+		{
+			shift[i][j].path_number = 0;
+		}
+	}
+	ifstream in;
+	in.open("C://Users/84101/Desktop/tabledata.txt");
+	if (in.is_open())
+	{
+		while (!in.eof())
+		{
+			string from, to;
+			in >> from >> to;
+			if (from == "北京") i = 0;
+			else if (from == "上海") i = 1;
+			else if (from == "广州") i = 2;
+			else if (from == "厦门") i = 3;
+			else if (from == "重庆") i = 4;
+			else if (from == "拉萨") i = 5;
+			else if (from == "乌鲁木齐") i = 6;
+			else if (from == "武汉") i = 7;
+			else if (from == "南京") i = 8;
+			else if (from == "哈尔滨") i = 9;
+			if (to == "北京") j = 0;
+			else if (to == "上海") j = 1;
+			else if (from == "广州") j = 2;
+			else if (from == "厦门") j = 3;
+			else if (from == "重庆") j = 4;
+			else if (from == "拉萨") j = 5;
+			else if (from == "乌鲁木齐") j = 6;
+			else if (from == "武汉") j = 7;
+			else if (from == "南京") j = 8;
+			else if (from == "哈尔滨") j = 9;
+			in >> shift[i][j].path[shift[i][j].path_number].traffic >> shift[i][j].path[shift[i][j].path_number].begin_time >> shift[i][j].path[shift[i][j].path_number].end_time >> shift[i][j].path[shift[i][j].path_number].cost >> shift[i][j].path[shift[i][j].path_number].time_used;
+			shift[i][j].path_number++;
+		}
 	}
 	else
 	{
-		for (i = 0; i < 10; i++)
-		{
-			for (j = 0; j < 10; j++)
-			{
-				fin >> shift[i][j].path_number;
-			}
-		}
+		cout << "Erroer:open txt failed" << endl;
 	}
-	fin.close();
-	int n = 0;
-	ifstream fins("tabledata.txt");
-	if (!fins.is_open())
+	in.close();
+	for (i = 0; i < 10; i++)
 	{
-		cout << "Error: opening file fail" << endl;
-		exit(1);
-	}
-	else
-	{
-		for (i = 0; i < 10; i++)
+		for (j = 0; j < 10; j++)
 		{
-			for (j = 0; j < 10; j++)
-			{
-				for (int x = 0; x < shift[i][j].path_number; x++)
-				{
-					in >> shift[i][j].path[n].traffic >> shift[i][j].path[n].from >> shift[i][j].path[n].begin_time >> shift[i][j].path[n].to >> shift[i][j].path[n].end_time >> shift[i][j].path[n].cost >> shift[i][j].path[n].time_used;
-					n++;
-				}
-			}
+			cout << " " << i << " " << j << " " << shift[i][j].path_number;
 		}
+		cout << endl;
 	}
-	fins.close();
 }
